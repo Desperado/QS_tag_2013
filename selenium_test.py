ddcfac3b9545de80ff634212ddda3cdcedfd6573
Ruslan Strazhnyk <ruslan@maven.co>
2013-08-31 15:42:59 +0300

    Changed to chromedriver

--- selenium_test.py
+++ selenium_test.py
@@ -7,22 +7,19 @@ import unittest, time, re
 
 class Untitled(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://www.qs-tag.de/"
+        self.base_url = "http://www.imbus.de/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
     def test_untitled(self):
         driver = self.driver
-        driver.get(self.base_url + "/home/")
-        driver.find_element_by_link_text("07. November 2013").click()
-        driver.find_element_by_xpath("//div[@id='c16188']/div/div/div[3]/p/a/strong").click()
-        self.assertEqual("Full Featured Test Automation using Selenium Grid and CI (Jenkins)", driver.find_element_by_css_selector("h1").text)
-        self.assertEqual("07.11.2013, 14:00 - 15:30 / Ruslan Strazhnyk, Maven Research Inc.", driver.find_element_by_css_selector("p").text)
-        driver.find_element_by_link_text("Veranstaltungsinfo").click()
-        self.assertEqual("Software-QS-Tag Archiv", driver.find_element_by_link_text("Software-QS-Tag Archiv").text)
+	driver.get(self.base_url + "/")
+        
+        driver.find_element_by_link_text("TestBench").click()
     
+	    
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException, e: return False
